954a2a47bd3042d876b24ca804fcbfd903853bf3
mcp613 <support@fullbrik.com>
2023-06-22 13:49:45 -0700

    more work on updater

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import importlib
+
 import gui
 import updates
 
 if __name__ == '__main__':
     if updates.check_update():
         updates.download_update()
+        importlib.reload(gui)
 
     gui.show_gui()
 
